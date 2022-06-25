9b4f85dcdc5b41c0ed385dcfa2f06e5c94bdad39
Diego <34724728+Calcoph@users.noreply.github.com>
2022-06-25 09:14:58 +0200

    add example font

--- te-mapmaker/src/mapmaker.rs
+++ te-mapmaker/src/mapmaker.rs
@@ -357,9 +357,9 @@ impl RendererState {
                         resolve_target: None,
                         ops: wgpu::Operations {
                             load: wgpu::LoadOp::Clear(wgpu::Color {
-                                r: 0.1,
-                                g: 0.6,
-                                b: 0.2,
+                                r: 0.0,
+                                g: 0.0,
+                                b: 0.0,
                                 a: 1.0
                             }),
                             store: true
