4462df0675758cb2f38f9ba8de54f0c39a379f0e
Diego <34724728+Calcoph@users.noreply.github.com>
2022-07-02 13:30:50 +0200

    start animated sprite

--- te-renderer/src/instances/sprite.rs
+++ te-renderer/src/instances/sprite.rs
@@ -154,3 +154,111 @@ impl InstancedDraw for InstancedSprite {
         );
     }
 }
+
+pub struct AnimatedSprite {
+    pub sprite: model::Material,
+    width: f32,
+    height: f32,
+    pub instance: Instance2D,
+    pub instance_buffer: wgpu::Buffer,
+    pub depth: f32,
+}
+
+impl AnimatedSprite {
+    pub fn new(
+        sprite: model::Material,
+        device: &wgpu::Device,
+        x: f32,
+        y: f32,
+        depth: f32,
+        w: f32,
+        h: f32,
+    ) -> Self {
+        let mut instance = Instance2D {
+            position: cgmath::Vector2 { x, y },
+            size: cgmath::Vector2 { x: w, y: h },
+            animation: None
+        };
+
+        let instance_data = instance.to_raw();
+        let instance_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Instance Buffer"),
+            contents: bytemuck::cast_slice(&[instance_data]),
+            usage: wgpu::BufferUsages::VERTEX | wgpu::BufferUsages::COPY_DST,
+        });
+
+        AnimatedSprite {
+            sprite,
+            width: w,
+            height: h,
+            instance,
+            instance_buffer,
+            depth,
+        }
+    }
+
+    pub fn resize<V: Into<cgmath::Vector2<f32>>>(
+        &mut self,
+        index: usize,
+        new_size: V,
+        queue: &wgpu::Queue,
+    ) {
+        self.instance.resize(new_size);
+        let raw = self.instance.to_raw();
+        queue.write_buffer(
+            &self.instance_buffer,
+            (index * std::mem::size_of::<InstanceRaw>())
+                .try_into()
+                .unwrap(),
+            bytemuck::cast_slice(&[raw]),
+        );
+    }
+
+    pub(crate) fn animate(&mut self, queue: &wgpu::Queue) {
+        if self.instance.animation.is_some() {
+            queue.write_buffer(
+                &self.instance_buffer,
+                (0)
+                    .try_into()
+                    .unwrap(),
+                bytemuck::cast_slice(&[self.instance.to_raw()]),
+            );
+        }
+    }
+}
+
+impl InstancedDraw for AnimatedSprite {
+    fn move_instance<V: Into<cgmath::Vector3<f32>>>(
+        &mut self,
+        index: usize,
+        direction: V,
+        queue: &wgpu::Queue,
+    ) {
+        self.instance.move_direction(direction);
+        let raw = self.instance.to_raw();
+        queue.write_buffer(
+            &self.instance_buffer,
+            (index * std::mem::size_of::<InstanceRaw>())
+                .try_into()
+                .unwrap(),
+            bytemuck::cast_slice(&[raw]),
+        );
+    }
+
+    fn set_instance_position<P: Into<cgmath::Vector3<f32>>>(
+        &mut self,
+        index: usize,
+        position: P,
+        queue: &wgpu::Queue,
+    ) {
+        self.instance.move_to(position);
+        let raw = self.instance.to_raw();
+        queue.write_buffer(
+            &self.instance_buffer,
+            (index * std::mem::size_of::<InstanceRaw>())
+                .try_into()
+                .unwrap(),
+            bytemuck::cast_slice(&[raw]),
+        );
+    }
+}
